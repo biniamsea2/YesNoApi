77c393bea2c796c2672268371ddcfa716ecb0670
Biniam <Biniamt23@yahoo.com>
2020-12-02 22:57:56 -0800

    working on database

--- YesNoApp/YesNoApp/Models/Services/YesNoManager.cs
+++ YesNoApp/YesNoApp/Models/Services/YesNoManager.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using YesNoApp.Models.Interfaces;
 
 namespace YesNoApp.Models.Services
 {
-    public class YesNoManager
+    public class YesNoManager : IYesNo
     {
+        public Task<YesNo> GetSinlgeYesNoRandom(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<YesNo> GetYesNoByID(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
