b7991c9792478fbb72147abb171d2fcb1f021f0f
Biniam <Biniamt23@yahoo.com>
2020-12-03 00:35:22 -0800

    database seeded for no

--- YesNoApp/YesNoApp/Data/YesNoDbContext.cs
+++ YesNoApp/YesNoApp/Data/YesNoDbContext.cs
@@ -46,7 +46,39 @@ namespace YesNoApp.Data
                     YesOrNo = "Yes",
                     Gif = "https://media.tenor.com/images/e7776abf34c8e4eb31af76929d945a12/tenor.gif",
                 },
+                new YesNo
+                {
+                    ID = 6,
+                    YesOrNo = "No",
+                    Gif = "https://media4.giphy.com/media/JYZ397GsFrFtu/200.gif",
+                },
+                new YesNo
+                {
+                    ID = 7,
+                    YesOrNo = "No",
+                    Gif = "https://media4.giphy.com/media/LOEI8jsNKPmzdJYvhJ/200.gif",
+                },
+                new YesNo
+                {
+                    ID = 8,
+                    YesOrNo = "No",
+                    Gif = "https://media1.tenor.com/images/b6219b2a69b5e209dc394a01d3de61c7/tenor.gif?itemid=12337588",
+                },
+                new YesNo
+                {
+                    ID = 9,
+                    YesOrNo = "No",
+                    Gif = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoBLh_8czGn-xQXpPyjPKAG0ZnH9OUjB4uKw&usqp=CAU",
+                },
+                new YesNo
+                {
+                    ID = 10,
+                    YesOrNo = "No",
+                    Gif = "https://media0.giphy.com/media/YOkrK8agZLEk2cXeLi/200.gif",
+                });
+        }
 
-
+        //table
+        public DbSet<YesNo> YesNoTable { get; set; }
     }
 }
