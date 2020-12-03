df2669165130d3f95d4f966cb6440d6f2ce6e909
Biniam <Biniamt23@yahoo.com>
2020-12-02 22:29:02 -0800

    class added

--- YesNoApp/YesNoApp/Models/YesNo.cs
+++ YesNoApp/YesNoApp/Models/YesNo.cs
@@ -7,5 +7,8 @@ namespace YesNoApp.Models
 {
     public class YesNo
     {
+        public int ID { get; set; }
+        public string YesOrNo { get; set; }
+        public string Meme { get; set; }
     }
 }
