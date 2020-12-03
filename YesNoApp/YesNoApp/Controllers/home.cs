c983df0b894a3a6f326df4e8261f625d55d02288
Biniam <Biniamt23@yahoo.com>
2020-12-02 22:27:25 -0800

    started

---
+++ YesNoApp/YesNoApp/Controllers/home.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YesNoApp.Controllers
+{
+    public class home : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("Hello from controller");
+        }
+    }
+}
