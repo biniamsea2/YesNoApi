8800aaa55abb7755d6d098457247439e132b18d9
Biniam <Biniamt23@yahoo.com>
2020-12-02 22:31:10 -0800

    interface updated

--- YesNoApp/YesNoApp/Models/Interfaces/IYesNo.cs
+++ YesNoApp/YesNoApp/Models/Interfaces/IYesNo.cs
@@ -7,5 +7,9 @@ namespace YesNoApp.Models.Interfaces
 {
     interface IYesNo
     {
+        //will get a random yes no and one by ID from the database, needs an id because we will use that to grab a random record
+        Task<YesNo> GetSinlgeYesNoRandom(int id);
+        Task<YesNo> GetYesNoByID(int id);
+
     }
 }
